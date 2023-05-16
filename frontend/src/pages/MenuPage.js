efd03c05e9204f30e605553c8fbcd4452dcaafba
Peter Huynh <peterhyh@yahoo.com>
2023-05-16 15:40:23 -0500

    inital push

---
+++ frontend/src/pages/MenuPage.js
@@ -0,0 +1,13 @@
+
+import StirFriedNoodles from '../app/img/stirFriedNoodles.png';
+
+const MenuPage = () => {
+    return (
+        <div className='menuPageContainer'>
+            <h1>Menu</h1>
+            <img src={StirFriedNoodles} alt='' />
+        </div>
+    )
+};
+
+export default MenuPage;
